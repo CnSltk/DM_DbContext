a3865d794471a0479d5ac3f56ee8c5eae0d990f2
CnSltk <cansaltik1903@gmail.com>
2025-06-03 23:19:25 +0200

    New dtos added for account.

---
+++ src/DeviceManager.API/DTO/AccountCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTO;
+
+public class AccountCreateDto
+{
+    [Required]
+    [RegularExpression(@"^[^\d]\S*$", ErrorMessage = "Username must not start with a number.")]
+    [StringLength(100, MinimumLength = 1)]
+    public string Username { get; set; } = null!;
+    [Required]
+    [MinLength(12, ErrorMessage = "Password must be at least 12 characters.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).+$",
+        ErrorMessage = "Password must contain at least one lowercase, one uppercase, one digit and one symbol.")]
+    public string Password { get; set; } = null!;
+    [Required]
+    public int EmployeeId { get; set; }
+    public string? RoleName { get; set; }
+}
---
+++ src/DeviceManager.API/DTO/AccountDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.DTO;
+
+public class AccountDto
+{
+    public int    Id       { get; set; }
+    public string Username { get; set; } = null!;
+    public string RoleName { get; set; } = null!;
+    public int    EmployeeId { get; set; }
+}
