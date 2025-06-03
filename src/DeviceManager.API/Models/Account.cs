33e8aed6b837f5a72406e6847285ef6a53cb2734
CnSltk <cansaltik1903@gmail.com>
2025-06-03 23:18:13 +0200

    Account and Role added as models

---
+++ src/DeviceManager.API/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models;
+
+public class Account
+{
+    public int    Id           { get; set; }
+    public string Username     { get; set; } = null!; 
+    public byte[] PasswordHash { get; set; } = null!;
+    public byte[] PasswordSalt { get; set; } = null!;
+    public int      EmployeeId { get; set; }
+    public Employee Employee   { get; set; } = null!;
+    public int  RoleId { get; set; }
+    public Role Role   { get; set; } = null!;
+}
