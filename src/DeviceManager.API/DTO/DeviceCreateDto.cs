49196508e59d8d9169e9df0841f3889562184a7a
CnSltk <cansaltik1903@gmail.com>
2025-06-03 06:16:58 +0200

    DTO's changed.

--- src/DeviceManager.API/DTO/DeviceCreateDto.cs
+++ src/DeviceManager.API/DTO/DeviceCreateDto.cs
@@ -4,8 +4,12 @@ namespace WebApplication1.DTO;
 
 public class DeviceCreateDto
 {
-    [Required] public string Name { get; set; } = null!;
-    [Required] public string DeviceTypeName { get; set; } = null!;
-    [Required] public bool   IsEnabled { get; set; }
-    [Required] public object AdditionalProperties { get; set; } = new { };
+    [Required]
+    public string Name { get; set; } = null!;
+    [Required]
+    public string DeviceTypeName { get; set; } = null!;
+    [Required]
+    public bool IsEnabled { get; set; }
+    [Required]
+    public object? AdditionalProperties { get; set; }
 }
--- src/DeviceManager.API/DTO/DeviceDetailDto.cs
+++ src/DeviceManager.API/DTO/DeviceDetailDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace WebApplication1.DTO;
 
-public class DeviceDetailDto
+public class DeviceDetailsDto
 {
     public string DeviceTypeName { get; set; } = null!;
     public bool IsEnabled { get; set; }
     public object AdditionalProperties { get; set; } = new { };
-    public CurrentEmployeeDto? CurrentEmployee { get; set; }
+    public CurrentEmployeeDto? Employee { get; set; }
 }
