bdc0329f0917df54ea3f05a1ce85f72380db483a
CnSltk <cansaltik1903@gmail.com>
2025-06-04 08:38:53 +0200

    Account.cs modified

--- src/DeviceManager.API/Models/Account.cs
+++ src/DeviceManager.API/Models/Account.cs
@@ -1,13 +1,21 @@
-﻿namespace WebApplication1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
 
 public class Account
 {
-    public int    Id           { get; set; }
-    public string Username     { get; set; } = null!; 
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string Username{ get; set; } = null!;
+    [Required]
     public byte[] PasswordHash { get; set; } = null!;
+    [Required]
     public byte[] PasswordSalt { get; set; } = null!;
-    public int      EmployeeId { get; set; }
-    public Employee Employee   { get; set; } = null!;
-    public int  RoleId { get; set; }
-    public Role Role   { get; set; } = null!;
+    [Required]
+    public int EmployeeId { get; set; }
+    public  Employee Employee{ get; set; } = null!;
+    [Required]
+    public int RoleId { get; set; }
+    public Role Role{ get; set; } = null!;
 }
