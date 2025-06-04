4621add672214f9d6e6a916d6499acbc1d89cf88
CnSltk <cansaltik1903@gmail.com>
2025-06-04 08:39:06 +0200

    Role.cs modified

--- src/DeviceManager.API/Models/Role.cs
+++ src/DeviceManager.API/Models/Role.cs
@@ -4,7 +4,5 @@ public class Role
 {
     public int    Id   { get; set; }
     public string Name { get; set; } = null!;  // “Admin” or “User”
-
-    // Navigation
     public ICollection<Account> Accounts { get; set; } = new List<Account>();
 }
