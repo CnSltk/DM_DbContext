6219325f4a59e55df6cf0d0d55e1d6334902ee71
CnSltk <cansaltik1903@gmail.com>
2025-06-04 08:40:42 +0200

    New Dto for updating accounts

---
+++ src/DeviceManager.API/DTO/AccountUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTO;
+
+public class AccountUpdateDto
+{
+    [Required] public string RoleName    { get; set; } = null!;
+    public string? NewPassword { get; set; }
+}
