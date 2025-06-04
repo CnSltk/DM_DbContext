bac17812eb08ff42a1d6f6433501323ec3564623
CnSltk <cansaltik1903@gmail.com>
2025-06-04 09:59:23 +0200

    dto added for updating employees

---
+++ src/DeviceManager.API/DTO/EmployeeUpdateDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTO;
+
+public class EmployeeUpdateDto
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+    public string? MiddleName { get; set; }
+    [Required]
+    public string LastName { get; set; } = null!;
+    [Required]
+    public string PhoneNumber { get; set; } = null!;
+    [Required]
+    public string Email { get; set; } = null!;
+    [Required]
+    public decimal Salary { get; set; }
+    [Required]
+    public int PositionId { get; set; }
+}
